d0bd055727cfc19051334795e50ca66a9e704658
AMT98 <aswinmalla12@gmail.com>
2024-11-26 20:31:36 -0700

    Created project list

---
+++ src/assets/data/projectData.tsx
@@ -0,0 +1,37 @@
+export const projectData = [
+  {
+    id: '1',
+    imgUrl: '/codeThreads.PNG',
+    title: 'CodeThreads 🧑🏽‍💻',
+    liveSite: 'https://codethreads.netlify.app/',
+    github: 'https://github.com/CodeThreads-FSA-VP-AM/CodeThreads',
+  },
+  {
+    id: '2',
+    imgUrl: '/fitnessTracker.PNG',
+    title: 'Fitness Tracker 🧑🏽‍💻',
+    liveSite: 'https://fitnesstraker.netlify.app/',
+    github: 'https://github.com/AMT98/FitnessTracker_Front-End',
+  },
+  {
+    id: '3',
+    imgUrl: '/strangersThings.PNG',
+    title: 'Strangers Things 🧑🏽‍💻',
+    liveSite: 'https://st-amt98.netlify.app',
+    github: 'https://github.com/AMT98/Strangers-Things',
+  },
+  {
+    id: '4',
+    imgUrl: '/portfolioV2.PNG',
+    title: 'Portfolio Website 🧑🏽‍💻',
+    liveSite: 'https://aswinmalla.netlify.app',
+    github: 'https://github.com/AMT98/Portfolio-Aswin',
+  },
+  {
+    id: '5',
+    imgUrl: '/artProject.PNG',
+    title: 'Art Collector 🧑🏽‍💻',
+    liveSite: 'https://artcollectorfsa.netlify.app/',
+    github: 'https://github.com/AMT98/UNIV_Art_Collector_React_Starter',
+  },
+];
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import projectData from "../assets/data/projectData";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section id="projects">
+      <div className="container">
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="mb-7 sm:mb-0">
+            <h3 className="text-headingColor text-[2rem] font-[700]">
+              My recent projects
+            </h3>
+          </div>
+          <div className="flex gap-3">
+            <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
+              All
+            </button>
+            <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
+              Frontend Development
+            </button>
+            <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
+              Fullstack Development
+            </button>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 flex-wrap mt-12"></div>
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;

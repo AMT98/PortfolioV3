88eac2833f576345f0f0a06ecf5947df3b32e692
AMT98 <aswinmalla12@gmail.com>
2024-11-26 21:38:28 -0700

    Modal created

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,43 @@
-import React from "react";
-import projectData from "../assets/data/projectData";
+import React, { useState, useEffect } from "react";
+import { projectData } from "../assets/data/projectData";
+import Modal from "../utils/modal";
 
-const Projects = () => {
+const Projects: React.FC = () => {
+  const [nextItems, setNextItems] = useState(3);
+  const [projects, setProjects] = useState(projectData);
+  const [selectTab, setSelectTab] = useState("all");
+  const [showModal, setShowModal] = useState(false);
+  const [activeID, setActiveID] = useState(false);
+
+  const loadMoreHandler = () => {
+    setNextItems((prev) => prev + 3);
+  };
+
+  const showModalHandler = (
+    id: string | boolean | ((prevState: boolean) => boolean)
+  ) => {
+    setShowModal(true);
+    setActiveID(id);
+  };
+
+  useEffect(() => {
+    if (selectTab === "all") {
+      setProjects(projectData);
+    }
+    if (selectTab === "frontend development") {
+      const filteredData = projectData.filter(
+        (item) => item.category === "Frontend Development"
+      );
+      setProjects(filteredData);
+    }
+    if (selectTab === "fullstack development") {
+      const filteredData = projectData.filter(
+        (item) => item.category === "Fullstack Development"
+      );
+      setProjects(filteredData);
+    }
+  }, [selectTab]);
+
   return (
     <section id="projects">
       <div className="container">
@@ -12,19 +48,65 @@ const Projects = () => {
             </h3>
           </div>
           <div className="flex gap-3">
-            <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
+            <button
+              onClick={() => setSelectTab("all")}
+              className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]"
+            >
               All
             </button>
-            <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
-              Frontend Development
-            </button>
-            <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
+            <button
+              onClick={() => setSelectTab("fullstack development")}
+              className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]"
+            >
               Fullstack Development
+            </button>
+            <button
+              onClick={() => setSelectTab("frontend development")}
+              className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]"
+            >
+              Frontend Development
             </button>
           </div>
         </div>
-        <div className="flex items-center gap-4 flex-wrap mt-12"></div>
+        <div className="flex items-center gap-4 flex-wrap mt-12">
+          {projects.slice(0, nextItems)?.map((project, index) => (
+            <div
+              key={index}
+              className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg:w-[32.2%] relative z-[1]"
+            >
+              <figure>
+                <img
+                  className="rounded-[8px]"
+                  src={project.imgUrl}
+                  alt="projectImage"
+                />
+              </figure>
+              <div className="w-full h-full bg-primaryColor bg-opacity-40 absolute top-0 left-0 z-[5] hidden group-hover:block">
+                <div className="w-full h-full flex items-center justify-center">
+                  <button
+                    onClick={() => showModalHandler(project.id)}
+                    className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4 rounded-[8px] font-[500] ease-in duration-200"
+                  >
+                    See Details
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="text-center mt-6">
+          {nextItems < projects.length && projectData.length > 3 && (
+            <button
+              onClick={loadMoreHandler}
+              className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4 rounded-[8px] font-[500] ease-in duration-200"
+            >
+              Load More
+            </button>
+          )}
+        </div>
       </div>
+      {showModal && <Modal activeID={activeID} setShowModal={setShowModal} />}
     </section>
   );
 };

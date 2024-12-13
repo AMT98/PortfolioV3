0d7c5e31a70aab03ed37f3576aece6ed25bb7e8f
AMT98 <aswinmalla12@gmail.com>
2024-12-12 20:44:52 -0700

    Uploaded resume to /resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Resume: React.FC = () => {
-  return <div>Resume</div>;
+  return (
+    <div>
+      <iframe
+        src="aswinmalla_resume.pdf"
+        title="Resume"
+        width="100%"
+        height="1500px"
+      />
+    </div>
+  );
 };
 
 export default Resume;

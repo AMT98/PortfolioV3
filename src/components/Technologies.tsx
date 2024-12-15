affb99ba17a7414fffea2de2cfca2f82566ab905
AMT98 <aswinmalla12@gmail.com>
2024-12-14 18:49:53 -0700

    Added emojis to the technologies

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -33,15 +33,34 @@ const Technologies: React.FC = () => {
               <div className="p-8 text-3xl font-bold text-center border-b-4  rounded-md">
                 Front-end Development{" "}
               </div>
-              <ul className="w-full text-center text-sm">
-                <li className="border-b py-4 font-bold">HTML5</li>
-                <li className="border-b py-4 font-bold">CSS</li>
-                <li className="border-b py-4 font-bold">TailwindCSS</li>
-                <li className="border-b py-4 font-bold">JavaScript</li>
-                <li className="border-b py-4 font-bold">TypeScript</li>
-                <li className="border-b py-4 font-bold">ReactJs</li>
-                <li className="border-b py-4 font-bold">NextJS</li>
-                <li className="border-b py-4 font-bold">Vite</li>
+              <ul className="w-full text-center text-base font-bold">
+                <li className="border-b py-4 flex hover:text-primaryColor items-center justify-center">
+                  <span className="mr-2">🌐</span> HTML5
+                </li>
+                <li className="border-b py-4 flex hover:text-primaryColor items-center justify-center">
+                  <span className="mr-2">🎨</span> CSS
+                </li>
+                <li className="border-b py-4 flex hover:text-primaryColor items-center justify-center">
+                  <span className="mr-2">💨</span> TailwindCSS
+                </li>
+                <li className="border-b py-4 flex hover:text-primaryColor items-center justify-center">
+                  <span className="mr-2">📜</span> JavaScript
+                </li>
+                <li className="border-b py-4 flex hover:text-primaryColor items-center justify-center">
+                  <span className="mr-2">📘</span> TypeScript
+                </li>
+                <li className="border-b py-4 hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🔄</span> Redux
+                </li>
+                <li className="border-b py-4 flex hover:text-primaryColor items-center justify-center">
+                  <span className="mr-2">⚛️</span> ReactJS
+                </li>
+                <li className="border-b py-4 flex hover:text-primaryColor items-center justify-center">
+                  <span className="mr-2">🚀</span> NextJS
+                </li>
+                <li className="py-4 flex items-center justify-center hover:text-primaryColor">
+                  <span className="mr-2">⚡</span> Vite
+                </li>
               </ul>
             </motion.div>
           </div>
@@ -51,19 +70,39 @@ const Technologies: React.FC = () => {
               variants={fadeIn("up", "tween", 0.2, 1)}
               className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow"
             >
-              <div className="w-full p-8 text-3xl font-bold text-center border-b-4 rounded-md">
+              <div className="w-full p-8 text-3xl font-bold text-center border-b-4 border-gray-300 rounded-md">
                 Software and Tools
               </div>
               <div className="h-1 w-full font-bold my-0 py-0 rounded-t"></div>
 
               <ul className="w-full text-center text-base font-bold">
-                <li className="border-b py-4 font-bold">Git</li>
-                <li className="border-b py-4 font-bold">Github</li>
-                <li className="border-b py-4 font-bold">Google</li>
-                <li className="border-b py-4 font-bold">Stack Overflow</li>
-                <li className="border-b py-4 font-bold">ChatGPT</li>
-                <li className="border-b py-4 font-bold">Visual Studio Code</li>
-                <li className="border-b py-4 font-bold">Visual Studio</li>
+                <li className="border-b py-4 hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🔧</span> Git
+                </li>
+                <li className="border-b py-4 hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🐙</span> GitHub
+                </li>
+                <li className="border-b py-4 hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🌐</span> Google
+                </li>
+                <li className="border-b py-4 hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">💡</span> Stack Overflow
+                </li>
+                <li className="border-b py-4 hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🤖</span> ChatGPT
+                </li>
+                <li className="border-b py-4 hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">📬</span> Postman
+                </li>
+                <li className="border-b py-4 hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🛠️</span> Insomnia
+                </li>
+                <li className="border-b py-4 hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">💻</span> Visual Studio Code
+                </li>
+                <li className="py-4 flex hover:text-primaryColor items-center justify-center">
+                  <span className="mr-2">🛠️</span> Visual Studio
+                </li>
               </ul>
               <div className="flex items-center justify-center"></div>
             </motion.div>
@@ -74,14 +113,31 @@ const Technologies: React.FC = () => {
               variants={fadeIn("left", "tween", 0.2, 1)}
               className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow"
             >
-              <div className="p-8 text-3xl font-bold text-center border-b-4  rounded-md">
+              <div className="p-8 text-3xl font-bold text-center border-b-4 border-gray-300 rounded-t-md">
                 Back-End Development
               </div>
               <ul className="w-full text-center text-sm">
-                <li className="border-b py-4 font-bold">Ruby on Rails </li>
-                <li className="border-b py-4 font-bold">PostgreSQL</li>
-                <li className="border-b py-4 font-bold">Node.JS</li>
-                <li className="border-b py-4 font-bold">Express</li>
+                <li className="border-b py-4 font-bold hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🚀</span> Ruby on Rails
+                </li>
+                <li className="border-b py-4 font-bold hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🐘</span> PostgreSQL
+                </li>
+                <li className="border-b py-4 font-bold hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🌐</span> Node.JS
+                </li>
+                <li className="border-b py-4 font-bold hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🔗</span> Express
+                </li>
+                <li className="border-b py-4 font-bold hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">📡</span> RESTful API Design
+                </li>
+                <li className="border-b py-4 font-bold hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🔐</span> OAuth
+                </li>
+                <li className="py-4 font-bold hover:text-primaryColor flex items-center justify-center">
+                  <span className="mr-2">🛡️</span> JWT
+                </li>
               </ul>
               <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6"></div>
             </motion.div>

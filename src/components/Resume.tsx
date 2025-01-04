f5c0736a1f09489f4ecf20fc6d7d0c1f2cca16c8
AMT98 <aswinmalla12@gmail.com>
2025-01-03 19:58:43 -0700

    Updated resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -15,7 +15,7 @@ const Resume: React.FC = () => {
       <div className="flex flex-col justify-center items-center h-screen mt-24">
         <div className="rounded-xl flex gap-3 p-2 ">
           <a
-            href="/aswinmalla_resume.pdf"
+            href="/AswinMalla_Resume.pdf"
             download
             target="_blank"
             title="Download resume"
@@ -25,7 +25,7 @@ const Resume: React.FC = () => {
           </a>
         </div>
         <iframe
-          src="aswinmalla_resume.pdf"
+          src="AswinMalla_Resume.pdf"
           title="Resume"
           className="hidden md:block w-full sm:w-3/4 md:w-2/3 lg:w-full h-full border rounded-lg shadow-lg"
         />
